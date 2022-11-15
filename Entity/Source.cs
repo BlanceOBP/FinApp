25942af78d4298fcc786e8ca23ee28a0906e90b1
Александр <89993026+BlanceOBP@users.noreply.github.com>
2022-11-15 22:22:16 +0300

    Initial commit

---
+++ Entity/Source.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinApp.Entity
+{
+    public class Source
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public float Summary { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime DateOfCreate { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime DateOfEdit { get; set; }
+
+        public int SourceOfIncomeId { get; set; }
+    }
+}
