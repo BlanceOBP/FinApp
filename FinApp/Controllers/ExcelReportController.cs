9248e5ecf6de7100008cfac56458f6f792fe9159
Александр <89993026+BlanceOBP@users.noreply.github.com>
2023-04-03 21:24:33 +0300

    changes

---
+++ FinApp/Controllers/ExcelReportController.cs
@@ -0,0 +1,48 @@
+﻿using FinApp.Interfaces;
+using FinApp.MiddleEntity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinApp.Controllers
+{
+    [ApiController]
+    [Route("excelReport")]
+    public class ExcelReportController : BaseController
+    {
+        private readonly IExcelReportService _excelReportService;
+
+        public ExcelReportController(IExcelReportService excelReportService)
+        {
+            _excelReportService = excelReportService;
+        }
+
+        /// <summary>
+        /// Get income and expenses Excel report of the  user.
+        /// </summary>
+        /// <param name="moneyFlow">Period or time input.</param>
+        /// <returns>Excel report for period of time input/ </returns>
+        [Authorize(Roles = "Administrator,User")]
+        [HttpPost]
+        public async Task<IActionResult> GetExcelReport([FromQuery] MoneyFlow moneyFlow)
+        {
+            var userId = GetUserId();
+            var wb = await _excelReportService.GetReport(userId, moneyFlow);
+
+            return Ok(wb);
+        }
+
+        /// <summary>
+        /// Changes excel report.
+        /// </summary>
+        /// <param name="report">Excel report file with changes.</param>
+        [Authorize(Roles = "Administrator,User")]
+        [HttpPut]
+        public async Task<ActionResult> SetChangesFromExcel(IFormFile report)
+        {
+            var userId = GetUserId();
+            await _excelReportService.SetChangesFromExcel(userId, report);
+
+            return Ok();
+        }
+    }
+}
---
+++ FinApp/ExtensionOfExcelReport.cs
@@ -0,0 +1,36 @@
+﻿using ClosedXML.Excel;
+
+namespace FinApp
+{
+    public static class ExtensionOfExcelReport
+    {
+        /// <summary>
+        /// To report sheet adds a row.
+        /// </summary>
+        /// <param name="wb">Add row from WorkSheet.</param>
+        /// <returns></returns>
+        public static IXLWorksheet AddRow(this IXLWorksheet wb)
+        {
+            wb.Cell("A2").SetValue("ID");
+            wb.Cell("B2").SetValue("Name");
+            wb.Cell("C2").SetValue("Amount");
+            wb.Cell("D2").SetValue("Date");
+            wb.Range("A2:D2").Style.Font.Bold = true;
+
+            return wb;
+        }
+
+        /// <summary>
+        /// First cell set value.
+        /// </summary>
+        /// <param name="worksheet">Worksheet to set first cell value.</param>
+        /// <param name="value">Value to set in cell.</param>
+        /// <returns></returns>
+        public static IXLWorksheet SetFirstCell(this IXLWorksheet worksheet, string value)
+        {
+            worksheet.FirstCell().SetValue(value);
+
+            return worksheet;
+        }
+    }
+}
